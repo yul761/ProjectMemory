a43334f378ce251c027c12ad76ba4730c8ffa433
Yuchen Lin <yuchen.lin@versaterm.com>
2026-05-13 10:57:07 -0700

    feat(db): add SQLite lite mode schema and conditional Prisma client

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,14 @@
-import { PrismaClient } from "@prisma/client";
+import type { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+function createPrismaClient(): PrismaClient {
+  if (process.env["STATECORE_MODE"] === "lite") {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { PrismaClient: LiteClient } = require("../generated/client-lite");
+    return new LiteClient() as PrismaClient;
+  }
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const { PrismaClient: FullClient } = require("@prisma/client");
+  return new FullClient();
+}
+
+export const prisma = createPrismaClient();
